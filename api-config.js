bc4d9942820f67843a2e4934065b19a90c04e17d
boninstore <boninstore.order@gmail.com>
2026-06-20 16:51:17 +0700

    api-config.js

--- api-config.js
+++ api-config.js
@@ -1,1 +1,1 @@
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxZkLPxKcWdEjc3mpBf4AG-du6tnX7WuKoYNwDtkJhyVqaU_DdKKjrS-_oh6GKftunSlw/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbzgxnh4wGCUx7IvuS-nQcqm3I_a2mv1ua7FdA75XUFHE6JlpSQwHTC4TXcjdn1BXhhgSw/exec";
